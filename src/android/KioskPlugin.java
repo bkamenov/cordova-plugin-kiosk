451d3abb566b87f53f7dce46abb1c12ae82a9b28
bkamenov <44864901+bkamenov@users.noreply.github.com>
2025-08-25 21:00:15 +0300

    ALmost ready...

--- src/android/KioskPlugin.java
+++ src/android/KioskPlugin.java
@@ -4,20 +4,37 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-import android.view.View;
 import android.provider.Settings;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.MotionEvent;
 
 import org.apache.cordova.*;
 import org.json.JSONArray;
 
 public class KioskPlugin extends CordovaPlugin {
 
+  private View overlayView = null;
+  private WindowManager windowManager = null;
+
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
     switch (action) {
-      case "enable":
+      case "enableImmersiveMode":
         enableImmersiveMode();
         callbackContext.success("Immersive mode enabled");
+        return true;
+
+      case "disableImmersiveMode":
+        disableImmersiveMode();
+        callbackContext.success("Immersive mode disabled");
+        return true;
+
+      case "isLauncher":
+        isLauncher(callbackContext);
         return true;
 
       case "switchLauncher":
@@ -28,6 +45,20 @@ public class KioskPlugin extends CordovaPlugin {
       default:
         return false;
     }
+  }
+
+  private void isLauncher(CallbackContext callbackContext) {
+    Activity activity = cordova.getActivity();
+    Context context = activity.getApplicationContext();
+    Intent intent = new Intent(Intent.ACTION_MAIN);
+    intent.addCategory(Intent.CATEGORY_HOME);
+    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    String currentHomePackage = context.getPackageManager().resolveActivity(intent, 0).activityInfo.packageName;
+    String appPackage = context.getPackageName();
+
+    boolean isDefaultLauncher = currentHomePackage.equals(appPackage);
+    PluginResult result = new PluginResult(PluginResult.Status.OK, isDefaultLauncher);
+    callbackContext.sendPluginResult(result);
   }
 
   private void enableImmersiveMode() {
@@ -45,6 +76,18 @@ public class KioskPlugin extends CordovaPlugin {
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
       }
+    });
+  }
+
+  private void disableImmersiveMode() {
+    final Activity activity = cordova.getActivity();
+    cordova.getActivity().runOnUiThread(() -> {
+      activity.getWindow().clearFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+      View decorView = activity.getWindow().getDecorView();
+      decorView.setSystemUiVisibility(
+          View.SYSTEM_UI_FLAG_VISIBLE
+              | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
     });
   }
 
--- www/kiosk.js
+++ www/kiosk.js
@@ -1,9 +1,18 @@
 var exec = require('cordova/exec');
 
-exports.enable = function (success, error) {
-  exec(success, error, 'KioskPlugin', 'enable', []);
+exports.enableImmersiveMode = function (success, error) {
+  exec(success, error, 'KioskPlugin', 'enableImmersiveMode', []);
+};
+
+exports.disableImmersiveMode = function (success, error) {
+  exec(success, error, 'KioskPlugin', 'disableImmersiveMode', []);
+};
+
+exports.isLauncher = function (success, error) {
+  exec(success, error, 'KioskPlugin', 'isLauncher', []);
 };
 
 exports.switchLauncher = function (success, error) {
   exec(success, error, 'KioskPlugin', 'switchLauncher', []);
 };
+
